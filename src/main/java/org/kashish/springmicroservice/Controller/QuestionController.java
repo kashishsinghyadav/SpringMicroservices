7a5d2fc82b4fbe525709f32177e3f3dc4e5f1d31
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-05-02 21:48:07 +0530

    connect postgres and write first api

--- src/main/java/org/kashish/springmicroservice/Controller/QuestionController.java
+++ src/main/java/org/kashish/springmicroservice/Controller/QuestionController.java
@@ -1,8 +1,21 @@
 package org.kashish.springmicroservice.Controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/question")
 @RestController
 public class QuestionController {
+
+
+    @GetMapping("/getAllQuestion")
+    public String getAllQuestion(){
+        return "Hello Radhe radhe";
+    }
+
+
+
+
 }
